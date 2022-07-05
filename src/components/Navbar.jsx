c4e548aac89f015f1881b742bc26827caf0bb516
lswatson16 <lauralynw16@gmail.com>
2022-07-05 02:43:35 -0400

    Revise redundant image alt attribute

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return(
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0A192F] text-gray-300'>
       <div>
-        <img className='w-12' src={Logo} alt='logo image' />
+        <img className='w-12' src={Logo} alt='logo' />
       </div>
 
       {/* Nav menu */}
