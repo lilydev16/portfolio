006859fff537da4dabfb44f45ee8467962757643
lswatson16 <lauralynw16@gmail.com>
2022-07-05 04:23:37 -0400

    Add styling to the Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,12 +5,17 @@ const Home = () => {
   return(
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <h1>Hi, my name is</h1>
-        <h2>Lauralyn Watson</h2>
-        <h3>I'm a software engineer</h3>
-        <p>I'm a software engineer specialized in building inclusive and responsive web applications.</p>
+        <h1 className='text-pink-600'>Hi, my name is</h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Lauralyn Watson</h2>
+        <h3 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a software engineer</h3>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a software engineer specialized in building inclusive and responsive web applications.</p>
         <div>
-          <button>View Work < HiArrowNarrowRight /></button>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+            View Work
+            <span className='group-hover:rotate-90 duration-300'>
+              < HiArrowNarrowRight className='ml-3'/>
+            </span>
+          </button>
         </div>
       </div>
 
