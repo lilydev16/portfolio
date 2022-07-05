6fcedb7213ede912208fc465d1d6c4fc6c653cdd
lswatson16 <lauralynw16@gmail.com>
2022-07-05 03:54:18 -0400

    Add icons and links to GitHub, email and resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,6 +47,27 @@ const Navbar = () => {
                 <FaLinkedin size={30} />
               </a>
             </li>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+              <a className='flex justify-between items-center w-full text-gray-300' 
+                href='https://github.com/lswatson16'>
+                GitHub
+                <FaGithub size={30} />
+              </a>
+            </li>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+              <a className='flex justify-between items-center w-full text-gray-300' 
+                href='mailto:watsonlauralyn@gmail.com'>
+                Email
+                <HiOutlineMail size={30} />
+              </a>
+            </li>
+            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+              <a className='flex justify-between items-center w-full text-gray-300' 
+                href='https://www.linkedin.com/in/lauralyn-watson/'>
+                Resume
+                <BsFillPersonLinesFill size={30} />
+              </a>
+            </li>
           </ul>
         </div>
     </div>
