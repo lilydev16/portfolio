f8f86e7d93b25e88e318a16d03ef2ea70a442aa1
lswatson16 <lauralynw16@gmail.com>
2022-07-05 01:52:55 -0400

    Add logo and menus to the Navbar; add a hidden class to the hamburger and mobile view menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
+import Logo from '../assets/logo.png';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
   return(
-    <div>Navbar</div>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0A192F] text-gray-300'>
+      <div>
+        <img className='w-12' src={Logo} alt='logo image' />
+      </div>
+
+      {/* Nav menu */}
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      {/* Hamburger */}
+      <div className='hidden'>
+        <FaBars />
+      </div>
+
+      {/* Mobile menu */}
+        <div className='hidden'>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+
+      {/* Social icons */}
+        <div></div>
+    </div>
   )
 }
 
