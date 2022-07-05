6787971d8b0ca424f3cf4fd215009531daa06d7f
lswatson16 <lauralynw16@gmail.com>
2022-07-05 02:42:47 -0400

    Add state to toggle the hamburger and mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../assets/logo.png';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav)
+
   return(
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0A192F] text-gray-300'>
       <div>
@@ -19,12 +22,12 @@ const Navbar = () => {
       </ul>
 
       {/* Hamburger */}
-      <div className='md:hidden z-10'>
+      <div onClick={handleClick} className='md:hidden z-10'>
         <FaBars />
       </div>
 
       {/* Mobile menu */}
-      <ul className='absolute top-0 left-0 w-full h-screen bg-[#0A192F] flex flex-col justify-center items-center'>
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0A192F] flex flex-col justify-center items-center'}>
         <li className='py-6 text-4xl'>Home</li>
         <li className='py-6 text-4xl'>About</li>
         <li className='py-6 text-4xl'>Skills</li>
