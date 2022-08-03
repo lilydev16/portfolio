2879d886273485a73d8c619837071f44fb3c1756
lswatson16 <lauralynw16@gmail.com>
2022-08-03 12:11:25 -0400

    Add additional placeholders for projects in the grid container

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -13,9 +13,9 @@ const Work = () => {
         </div>
 
         {/* Container for all projects*/}
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'
-        >
-          {/* Container for a specific card/grid item */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+          
+          {/* Card/Grid item */}
           <div
             style={{ backgroundImage: `url(${tribe})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
@@ -35,6 +35,112 @@ const Work = () => {
               </div>
             </div>
           </div>
+          
+          {/* Grid item */}
+          <div
+            style={{ backgroundImage: `url(${rancid})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          > 
+            {/* Hover effects */}
+            <div className='opacity-0 group-hover:opacity-100'> 
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+        
+          {/* Grid item */}
+          <div
+            style={{ backgroundImage: `url(${tribe})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          > 
+            {/* Hover effects */}
+            <div className='opacity-0 group-hover:opacity-100'> 
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid item */}
+          <div
+            style={{ backgroundImage: `url(${tribe})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          > 
+            {/* Hover effects */}
+            <div className='opacity-0 group-hover:opacity-100'> 
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid item */}
+          <div
+            style={{ backgroundImage: `url(${tribe})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          > 
+            {/* Hover effects */}
+            <div className='opacity-0 group-hover:opacity-100'> 
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Grid item */}
+          <div
+            style={{ backgroundImage: `url(${tribe})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          > 
+            {/* Hover effects */}
+            <div className='opacity-0 group-hover:opacity-100'> 
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                </a>
+              </div>
+            </div>
+          </div>
+        
         </div>
       </div>
     </div>
