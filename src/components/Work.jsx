8e2f2239b662e66bb5fc21bd0eec4d48f2704fb4
lswatson16 <lauralynw16@gmail.com>
2022-08-03 11:09:08 -0400

    Add styling to grid container and grid item container

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,15 +3,19 @@ import React from "react";
 const Work = () => {
   return(
     <div name='work' className='w-full md:h-screen text-gray-300'>
+      {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
           <p className='py-6'>Check out some of my recent work</p>
         </div>
 
-        <div>
-          <div>
-            <div>
+        {/* Container for all projects*/}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+          {/* Container for a specific card/grid item */}
+          <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'> 
+            {/* Hover effects */}
+            <div> 
               <span>
 
               </span>
