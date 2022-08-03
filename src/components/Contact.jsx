2f89763af63a6abac9b46183a44387a1e32aff6e
lswatson16 <lauralynw16@gmail.com>
2022-08-03 12:53:49 -0400

    Update contact form to post new submissions to a URL/endpoint using getform.io

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Contact = () => {
   return (
     <div name='contact' className = 'w-full h-screen flex justify-center items-center p-4'>
-      <form action='' className='flex flex-col max-w-[600px] w-full'>
+      <form
+        action='https://getform.io/f/9aa474c1-f3e0-4cda-aa79-b8d275960d99'
+        method='POST'
+        className='flex flex-col max-w-[600px] w-full'
+      >
         <div className='pb-8'>
           <p className='text-gray-300 text-4xl font-bold inline border-b-4 border-pink-600'>Contact</p>
           <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - myemail@gmail.com</p>
